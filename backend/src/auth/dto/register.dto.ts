caca9ccb2afd7574243c5a52895e2697801963e3
Muhammad Mustafa Giani <bscs2312161@szabist.pk>
2026-07-14 17:53:55 +0500

    fix(backend): add role to register dto to pass validation

--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,4 +1,5 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsEnum, IsString, MinLength } from 'class-validator';
+import { UserRole } from '@prisma/client';
 
 export class RegisterDto {
   @IsEmail()
@@ -7,4 +8,7 @@ export class RegisterDto {
   @IsString()
   @MinLength(8)
   password!: string;
+
+  @IsEnum(UserRole)
+  role!: UserRole;
 }
